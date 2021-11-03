e0fbb89222151c1d5ca5e2132ffe8c2f8fb38f84
Abdul Said <abdulrahman.asaid@gmail.com>
2021-11-03 22:54:05 +0000

    added other keys to press

--- input.js
+++ input.js
@@ -16,20 +16,32 @@ const handleUserInput = function(key) {
     process.exit();
   }
   if (key === "w") {
-    console.log("Move: up")
-    connection.write("Move: up")
+    console.log("Move: up");
+    connection.write("Move: up");
   }
   if (key === "a") {
-    console.log("Move: left")
-    connection.write("Move: left")
+    console.log("Move: left");
+    connection.write("Move: left");
   }
   if (key === "s") {
-    console.log("Move: down")
-    connection.write("Move: down")
+    console.log("Move: down");
+    connection.write("Move: down");
   }
   if (key === "d") {
-    console.log("Move: right")
-    connection.write("Move: right")
+    console.log("Move: right");
+    connection.write("Move: right");
+  }
+  if (key === "k") {
+    connection.write("Say: John Cena");
+  }
+  if (key === "j") {
+    connection.write("Say: is the best");
+  }
+  if (key === "u") {
+    connection.write("Say: You Can't");
+  }
+  if (key === "c") {
+    connection.write("Say: No You Can't");
   }
 }
 
